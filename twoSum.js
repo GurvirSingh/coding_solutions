2f24dba0f8cfd0535c804a4f5b23e21ac8c265ac
Gurvir Singh Tarlok Singh Bhogal <40941950+GurvirSingh@users.noreply.github.com>
2021-01-13 14:01:29 -0600

    Add files via upload

--- twoSum.js
+++ twoSum.js
@@ -1,14 +1,15 @@
-const twoSum = (array, targetValue) => {
+// T- O(n), S-O(n)
+
+const twoSum = (array, target) => {
     const check = {};
 
     for(let i = 0; i < array.length; i++) {
         if(check[array[i]] >= 0) {
             return [array[i], array[check[array[i]]]];
         }
-        check[targetValue - array[i]] = i;
+        check[target - array[i]] = i;
     }
-};
+    return [];
+}
 
 console.log(twoSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
-console.log(twoSum([4, 6], 10));
-console.log(twoSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17));
