f42e71f40c3259b35ed19b664b0fea7bd110c784
Gurvir Singh Tarlok Singh Bhogal <40941950+GurvirSingh@users.noreply.github.com>
2021-01-19 12:09:23 -0600

    Add files via upload

---
+++ threeLargestNo.js
@@ -0,0 +1,26 @@
+const threeLargesNo = (array) => {
+    const triplets = [-Infinity, -Infinity, -Infinity];
+
+    for(const el of array) {
+        if(el > triplets[2]) {
+            shiftAndAdd(el, 2, triplets);
+        } else if(el > triplets[1]) {
+            shiftAndAdd(el, 1, triplets);
+        } else if(el > triplets[0]) {
+            shiftAndAdd(el, 0, triplets);
+        }
+    }
+    return triplets;
+};
+
+const shiftAndAdd = (el, shift, triplets) => {
+    for(let i = 0; i <= shift; i++) {
+        if(shift == i) {
+            triplets[i] = el;
+        } else {
+            triplets[i] = triplets[i + 1];
+        }
+    }
+};
+
+console.log(threeLargesNo([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]));
